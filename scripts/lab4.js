60a2e15d4bcde7a343c7255677aac4aeef6a566f
Joseph <josephdavideddy@gmail.com>
2025-05-01 01:22:07 -0700

    Fixed sumValues and discountPrices functions

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,13 +7,8 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
-
-        result = num1 + num2;
-
-        return result;
-    }
-    else {
+        return num1 + num2;
+    } else {
         return !add;
     }
 }
@@ -25,11 +20,13 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    const discounted = []
+    if (prices.length === 0) return false;
+    
+    const discounted = [];
     const length = prices.length;
-    let discountedPrice = 0
-    for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+    
+    for (let i = 0; i < length; i++) {
+        const discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
